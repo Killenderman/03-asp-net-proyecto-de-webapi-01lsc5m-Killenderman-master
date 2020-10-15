ab953216d86d5aa30c09ab07f6276a7bd96776df
Killenderman <Killenderman eleazarpb12@gmail.com>
2020-10-15 14:50:29 -0600

    19. ASP.NET Web API
    - Validation Model

--- myapi/Controllers/UserController.cs
+++ myapi/Controllers/UserController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 using myapi.Models;
 
@@ -18,6 +19,9 @@ namespace myapi.Controllers
       _db = db;
     }
 
+    public bool UserExists(int id) =>
+      _db.Users.Any(u => u.Id == id);
+
     // select * from users;
     [HttpGet]
     public async Task<ActionResult<IEnumerable<User>>> GetAll()
@@ -27,13 +31,82 @@ namespace myapi.Controllers
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<User>> GetById(int id)
+    public async Task<ActionResult<User>> GetById([FromRoute] int id, [BindRequired, FromQuery] string otro)
     {
       var user = await _db.Users.FindAsync(id);
       if (user == null)
       {
         return NotFound();
       }
+      System.Console.WriteLine("otro={0}", otro);
+      return user;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateNew([FromBody] User newUser)
+    {
+      _db.Users.Add(newUser);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetById), new { id = newUser.Id }, newUser);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> SaveChanges([FromRoute] int id, [FromBody] User modifiedUser)
+    {
+      if (id != modifiedUser.Id)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(modifiedUser).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!UserExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<User>> Delete([FromBody] int id)
+    {
+      var user = await _db.Users.FindAsync(id);
+      if (user == null)
+      {
+        return NotFound();
+      }
+
+      _db.Users.Remove(user);
+      await _db.SaveChangesAsync();
+
+      return user;
+    }
+
+    [HttpPost]
+    [Route("/api/login")]
+    public async Task<ActionResult<User>> Login([FromForm] string name, [FromForm] string password)
+    {
+      System.Console.WriteLine($"name={name} password={password}");
+      // TODO: Definir la consulta a la tabla users para validar el login
+      var user = await _db.Users.FirstOrDefaultAsync(u => u.Name == name && u.Password == password);
+      if (user == null)
+      {
+        return NotFound();
+      }
+      
       return user;
     }
   }
